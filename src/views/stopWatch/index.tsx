0baef72291c8878e41594b50c7b770a1181ef9df
ixio0330 <ixio0330@gmail.com>
2022-10-28 19:51:39 +0900

    [Feat] stop watch view 다국어처리

--- src/views/stopWatch/index.tsx
+++ src/views/stopWatch/index.tsx
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
 import useStopWatch, { StopWatchStatus } from '../../hooks/useStopWatch';
+import { useTranslation } from 'react-i18next';
+import '../../i18n';
 
 export default function StopWatchView() {
   const [status, setStatus] = useState('reset' as StopWatchStatus);
   const { watch } = useStopWatch({ status });
+  const { t } = useTranslation();
 
   function handleStatus(status: StopWatchStatus) {
     setStatus(status);
   }
 
+  const startEl = t('start');
+  const restartEl = t('restart');
+  const stopEl = t('stop');
+  const resetEl = t('reset');
+
   const buttonElement = 
-  status === 'reset' ? <button onClick={() => handleStatus('start')}>시작</button> : 
-  status === 'start' ? <button onClick={() => handleStatus('stop')}>중지</button> : 
+  status === 'reset' ? <button onClick={() => handleStatus('start')}>{startEl}</button> : 
+  status === 'start' ? <button onClick={() => handleStatus('stop')}>{stopEl}</button> : 
   <>
-    <button onClick={() => handleStatus('start')}>재시작</button>
-    <button onClick={() => handleStatus('reset')}>초기화</button>
+    <button onClick={() => handleStatus('reset')}>{resetEl}</button>
+    <button onClick={() => handleStatus('start')}>{restartEl}</button>
   </>
 
   return (
